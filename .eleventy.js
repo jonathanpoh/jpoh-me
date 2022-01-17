b3b5bb7d96851dae6da142bb8228da22d844d5cd
Jonathan Poh <me@jonathanpoh.com>
2022-01-17 14:27:55 +0000

    Add Gravatar plugin for eleventy

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
-module.exports = function(eleventyConfig) {
+const gravatarPlugin = require('eleventy-plugin-gravatar');
+
+module.exports = (eleventyConfig) => {
   eleventyConfig.setUseGitIgnore(false);
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   eleventyConfig.addPassthroughCopy('./src/img');
+  eleventyConfig.addPlugin(gravatarPlugin);
+     
   return {
     dir: {
       input: 'src',
