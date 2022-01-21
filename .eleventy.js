d2a48062779c2570ceae1af3d4a617d274cac12b
Jonathan Poh <me@jonathanpoh.com>
2022-01-21 00:10:46 +0000

    Add robots.txt and favicons

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,12 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addWatchTarget('./src/css/tailwind.css');
   eleventyConfig.addPassthroughCopy('./src/img');
   eleventyConfig.addPassthroughCopy('./src/js');
+  eleventyConfig.addPassthroughCopy('./src/robots.txt');
+  eleventyConfig.addPassthroughCopy('./src/site.webmanifest');
+  eleventyConfig.addPassthroughCopy('./src/browserconfig.xml');
+  eleventyConfig.addPassthroughCopy('./src/favicon.ico');
+  eleventyConfig.addPassthroughCopy('./src/*.png');
+  eleventyConfig.addPassthroughCopy('./src/*.svg');
   eleventyConfig.addPlugin(gravatarPlugin);
   eleventyConfig.addShortcode("currentYear", function() { currentYear = new Date().getFullYear(); return currentYear;})
   return {
